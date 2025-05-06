813e39c363f15238f33a8d39dca2feeeaa17bb56
Wildan Muhammad Athoillah <wildantechnoads77@gmail.com>
2025-05-06 15:04:39 +0700

    add buildSrc module

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,7 @@
+object Version {
+    const val OKHTTP_LOG_VERSION = "5.0.0-alpha.3"
+}
+
+object AndroidDependencies {
+    const val HTTP_LOG = "com.squareup.okhttp3:logging-interceptor:${Version.OKHTTP_LOG_VERSION}"
+}
